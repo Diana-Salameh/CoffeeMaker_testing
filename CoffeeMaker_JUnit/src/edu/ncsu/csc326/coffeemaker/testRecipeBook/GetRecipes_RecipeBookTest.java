86c1af39c8c0a2b8acc631d2cf37eb3204432ce6
dianasalameh <dianasalameh@localhost>
2021-03-28 23:32:58 +0300

    Commit GetRecipesTest

--- CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/testRecipeBook/GetRecipes_RecipeBookTest.java
+++ CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/testRecipeBook/GetRecipes_RecipeBookTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.ncsu.csc326.coffeemaker.Recipe;
+import edu.ncsu.csc326.coffeemaker.RecipeBook;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+
 public class GetRecipes_RecipeBookTest {
 
-	@Test
-	public void testGetRecipes() {
-		fail("Not yet implemented");
+	@Test(expected = NullPointerException.class)
+	public void testGetRecipes() throws RecipeException {
+		Recipe expected = new Recipe();
+		Recipe objRecipe = new Recipe();
+		Recipe actual [] ;
+
+		RecipeBook objRecipeBook = new RecipeBook();
+		objRecipeBook.addRecipe(objRecipe);
+		actual = objRecipeBook.getRecipes();
+		assertEquals(expected, actual);
+
 	}
 
 }
